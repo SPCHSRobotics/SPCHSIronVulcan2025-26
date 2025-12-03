1dd5b55053ed7e07924716b8ceeea454c7d16ac0
Glez Bubov <156974583+An-individuall@users.noreply.github.com>
2025-12-03 14:58:38 -0500

    Trying to fix out github 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapping/GyroScope.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapping/GyroScope.java
@@ -17,17 +17,6 @@ public class GyroScope {
 
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
 
-        double yawAngle360 = orientation.getYaw((AngleUnit.DEGREES));
-        // ^ Is between -180 and 180. Positive turns left, negative turns right
-        if (yawAngle360 > 0){
-            //Do nothing
-        } else if (yawAngle360 < 0){
-            yawAngle360 = (yawAngle360 + 360);
-        } else {
-            //This line is redundant since this situation only occurs when yawAngle360 == 0 or 360
-            yawAngle360 = 0;
-        }
-
-        return Math.toRadians(yawAngle360);
+        return orientation.getYaw(AngleUnit.DEGREES);
     }
 }
